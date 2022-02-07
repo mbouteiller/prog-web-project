6638755821f57f60fe138eadcd635014df06bb42
vivianlosciale <vivian.losciale@etu.univ-cotedazur.fr>
2022-02-07 15:34:56 +0100

    added function to get station from filters

--- back/src/controllers/station.controller.ts
+++ back/src/controllers/station.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get, Query } from "@nestjs/common";
+import { Body, Controller, Get, Query } from '@nestjs/common';
 import { StationService } from '../services/station.service';
 import { Station } from '../schemas/station.schema';
+import { StationEntriesDto } from '../dtos/station_entries.dto';
 
-@Controller()
+@Controller('station')
 export class StationController {
   constructor(private readonly appService: StationService) {}
 
-  @Get()
+  @Get('test')
   test(@Query('cp') cp: string): Promise<Station[]> {
-    if(cp) return this.appService.getAllFromCp(cp);
+    if (cp) return this.appService.getAllFromCp(cp);
     else return this.appService.getAll();
+  }
+
+  @Get()
+  GetStationFromFilter(@Body() filter: StationEntriesDto) {
+    return this.appService.getWithFilter(filter);
   }
 }
